901192227c94d0e6f60de08849d22c89db26902e
FabricioCoimbra <fabriciob.coimbra@gmail.com>
2018-11-23 22:17:36 -0200

    Adicionada connectionstring do bd

--- SimpraoSistemas/SimpraoSistemas/Startup.cs
+++ SimpraoSistemas/SimpraoSistemas/Startup.cs
@@ -27,6 +27,7 @@ namespace SimpraoSistemas
 			services.Configure<CookiePolicyOptions>(options =>
 			{
 				// This lambda determines whether user consent for non-essential cookies is needed for a given request.
+				//hello world
 				options.CheckConsentNeeded = context => true;
 				options.MinimumSameSitePolicy = SameSiteMode.None;
 			});
