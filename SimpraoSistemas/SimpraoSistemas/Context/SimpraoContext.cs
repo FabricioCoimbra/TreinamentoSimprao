4bcfa72f13f60a0cde85341e34a339ddac03e081
FabricioCoimbra <fabriciob.coimbra@gmail.com>
2018-11-24 00:16:04 -0200

    Conecta ou cria o banco de dados se não existir

---
+++ SimpraoSistemas/SimpraoSistemas/Context/SimpraoContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpraoSistemas.Models;
+
+namespace SimpraoSistemas.Context
+{
+	public class SimpraoContext : DbContext
+	{
+		public SimpraoContext()
+		{
+		}
+
+		public SimpraoContext(DbContextOptions<SimpraoContext> options) : base(options)
+		{
+			Database.EnsureCreated();
+		}
+
+		public virtual DbSet<Login> Login { get; set; }
+	}
+}
--- SimpraoSistemas/SimpraoSistemas/Startup.cs
+++ SimpraoSistemas/SimpraoSistemas/Startup.cs
@@ -2,8 +2,10 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SimpraoSistemas.Context;
 
 namespace SimpraoSistemas
 {
@@ -18,12 +20,16 @@ namespace SimpraoSistemas
 
 		public void ConfigureServices(IServiceCollection services)
 		{
+			var sqlConnectionString = Configuration.GetConnectionString("DataAccessPostgreSqlProvider");
+
+			services.AddDbContext<SimpraoContext>(opt => opt.UseNpgsql(sqlConnectionString));
+
+
 			services.Configure<CookiePolicyOptions>(options =>
 			{
 				options.CheckConsentNeeded = context => true;
 				options.MinimumSameSitePolicy = SameSiteMode.None;
 			});
-
 
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 		}
